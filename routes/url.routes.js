fc595e45408169f2e78ac703ed34315925935476
himanshuverma8 <hvinprimary@gmail.com>
2025-11-05 01:21:44 +0530

    add codes api endpoint to fetch all codes from urlTables

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -9,10 +9,23 @@ import { ensureAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+//get all the codes
+
+router.get("/codes", ensureAuthenticated,  async (req, res) => {
+  try {
+    const codes = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.userId, req.user.id));
+    return res.status(200).json({codes})
+  } catch (error) {
+    return res.status(500).json({error: 'Failed to fetch URLs'})
+  }
+});
+
 //shorten post request
 
 router.post("/shorten", ensureAuthenticated, async (req, res) => {
-
   const validationResult = await shortenPostRequestBodySchema.safeParseAsync(
     req.body
   );
@@ -36,28 +49,29 @@ router.post("/shorten", ensureAuthenticated, async (req, res) => {
       shortCode: urlsTable.shortCode,
       targetURL: urlsTable.targetURL,
     });
-  return res
-    .status(201)
-    .json({
-      id: result.id,
-      shortCode: result.shortCode,
-      targetURL: result.targetURL,
-    });
+  return res.status(201).json({
+    id: result.id,
+    shortCode: result.shortCode,
+    targetURL: result.targetURL,
+  });
 });
 
-router.get('/:shortCode', async (req, res) => {
+router.get("/:shortCode", async (req, res) => {
   const code = req.params.shortCode;
-  const [result] = await db.select({
-    targetURL: urlsTable.targetURL
-  })
-  .from(urlsTable)
-  .where(eq(urlsTable.shortCode,code));
+  const [result] = await db
+    .select({
+      targetURL: urlsTable.targetURL,
+    })
+    .from(urlsTable)
+    .where(eq(urlsTable.shortCode, code));
 
-  if(!result){
-    return res.status(404).json({error: 'invalid url'})
+  if (!result) {
+    return res.status(404).json({ error: "invalid url" });
   }
 
   return res.redirect(result.targetURL);
-})
+});
+
+
 
 export default router;
